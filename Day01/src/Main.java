1f525fa5ae84b29d6b404f81cec55220869cc8cd
Ben Scholer <benscholer3248511@gmail.com>
2016-12-10 22:32:57 -0500

    Started on day1

---
+++ Day01/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Created by bscholer on 12/9/16.
+ */
+public class Main {
+	
+	public static void main (String [] args) {
+		
+	}
+	
+	/**
+	 * Adds directions
+	 * @param directions R3, L8
+	 * @return (x, y)
+	 */
+	public static int [] findDestination(String directions) {
+		int [] final = new int[2];
+		return final;
+	}
+}
